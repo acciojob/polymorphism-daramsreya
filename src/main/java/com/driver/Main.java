937f75060e2b1fb7d0f2c1141427f5911e504d26
daramsreya83@gmail.com <your.email@example.com>
2025-05-14 21:30:33 +0530

    product is created

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(45,678,45));
+        System.out.print(p.product(4.56,5.78));
+    }
 }
